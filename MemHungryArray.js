e50e7aab5c137a11db2259e79a0d9b28a41c01bd
Bhaskar Gyan Vardhan <bhaskar.vardhan@walmartlabs.com>
2019-07-04 08:30:39 +0530

    add patch-package for react-native-gesture-handler

--- MemHungryArray.js
+++ MemHungryArray.js
@@ -24,29 +24,13 @@ class MemoryHungryArray extends Component {
   render() {
     const { record } = this.state;
     const last = record[record.length - 1];
-    var abc;
-    debugger;
-    abc = last[last.length - 1];
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{JSON.stringify(this.state.record.length)}</Text>
-        <Button title="Add Record Dude" onPress={() => this.addRecord()} />
-        <Button
-          title="Go back to Screen 1"
-          onPress={() =>
-            this.props.navigation.navigate('ScreenOne', { key: null })
-          }
-        />
+        <Text>Record length : {JSON.stringify(this.state.record.length)}</Text>
+        <Button title="Add Record" onPress={() => this.addRecord()} />
+
         <Button title="Remove" onPress={() => this.setState({ record: [] })} />
-        <Button
-          title="Pop to top"
-          onPress={() => this.props.navigation.navigate('HomeCart')}
-        />
-        <ScrollView>
-          <View>
-            <Text>{abc}</Text>
-          </View>
-        </ScrollView>
       </View>
     );
   }
